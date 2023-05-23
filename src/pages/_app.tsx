71ec27fe44f967fceef84db74ee30acaa710b865
Curtis Upshall <curtisupshall@gmail.com>
2023-05-23 13:21:06 -0700

    Created auth context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 import '../assets/styles/main.scss'
 
 import type { AppProps } from 'next/app';
- 
+import { AuthContextProvider } from '../contexts/AuthContext';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<AuthContextProvider>
+			<Component {...pageProps} />
+		</AuthContextProvider>
+	)
 }
