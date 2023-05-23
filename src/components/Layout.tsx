a996c0b154d32c03671381ce342dc6569a8b9387
SlaterRGordon <slats1999@gmail.com>
2023-05-23 11:08:14 -0700

    Add navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,9 @@
+import { Navigation } from "./navigation/Navigation"
+
 export default function Layout({ children }) {
     return (
         <>
+            <Navigation />
             <main>{children}</main>
         </>
     );
