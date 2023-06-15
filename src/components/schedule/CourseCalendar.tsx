36eeda4efe55acc8789d7b7a2f638a3d28973476
Rebecca Marshall <rebeccamarshall@uvic.ca>
2023-06-15 13:13:27 -0700

    add event detail modal

--- src/components/schedule/CourseCalendar.tsx
+++ src/components/schedule/CourseCalendar.tsx
@@ -1,30 +1,32 @@
-import { EventObject, TZDate } from "@toast-ui/calendar";
+import { EventObject, ExternalEventTypes, TZDate } from "@toast-ui/calendar";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Select from '@mui/material/Select';
-import { SelectChangeEvent } from '@mui/material/Select';
-import Stack from '@mui/material/Stack';
-import { MenuItem, Typography } from "@mui/material";
-import { createTheme } from '@mui/material/styles';
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Select from "@mui/material/Select";
+import { SelectChangeEvent } from "@mui/material/Select";
+import Stack from "@mui/material/Stack";
+import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import { createTheme } from "@mui/material/styles";
 import {
   convertIntoReadableMonth,
   convertIntoReadableRange,
   useSmallScreen,
-  createCalendarEvents,
-  mergeCourses,
+  convertToTime,
 } from "../../utils/helper";
 import { ViewType } from "../../types/calendar";
-import { Course } from "../../types/course"
-import Calendar from '@toast-ui/react-calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+import { Course } from "../../types/course";
+import Calendar from "@toast-ui/react-calendar";
+import "@toast-ui/calendar/dist/toastui-calendar.min.css";
 import { ThemeProvider } from "@emotion/react";
+import EventDetailModal from "./EventDetailModal";
+import { calendarColours } from "../common/sampleData/courseSchedule";
 
 const buttoneGroupTheme = createTheme({
   palette: {
     primary: {
-      main: '#000000',
+      main: "#000000",
     },
   },
 });
@@ -47,33 +49,39 @@ const CourseCalendar = ({
   view: ViewType;
   courses: Course[];
 }) => {
-  const calCourses = useMemo(() => createCalendarEvents(mergeCourses(courses)), [courses]);
+  const calCourses = useMemo(
+    () => createCalendarEvents(mergeCourses(courses)),
+    [courses]
+  );
 
   const calendarRef = useRef<typeof Calendar>(null);
   const [selectedDateRangeText, setSelectedDateRangeText] = useState("");
   const [selectedView, setSelectedView] = useState(view);
+  const [selectedEvent, setSelectedEvent] = useState<EventObject | null>(null);
 
   const isSmallScreen = useSmallScreen();
 
-  const initialCalendars = [{
-    id: "1",
-    name: "calendar-1",
-    color: "#ffffff",
-    // bgColor: "#9e5fff",
-    // dragBgColor: "#9e5fff",
-  }];
+  const initialCalendars = [
+    {
+      id: "1",
+      name: "calendar-1",
+      color: "#ffffff",
+      // bgColor: "#9e5fff",
+      // dragBgColor: "#9e5fff",
+    },
+  ];
 
   const useDisclosure = () => {
     const [isOpen, setIsOpen] = useState(false);
-  
+
     const onOpen = () => {
       setIsOpen(true);
     };
-  
+
     const onClose = () => {
       setIsOpen(false);
     };
-  
+
     return { isOpen, onOpen, onClose };
   };
 
@@ -143,6 +151,15 @@ const CourseCalendar = ({
     setSelectedView(event.target.value as ViewType);
   };
 
+  const onClickEvent: ExternalEventTypes["clickEvent"] = (res) => {
+    console.group("onClickEvent");
+    console.log("MouseEvent : ", res.nativeEvent);
+    console.log("Event Info : ", res.event);
+    console.groupEnd();
+    setSelectedEvent(res.event);
+    onEventDetailOpen();
+  };
+
   useEffect(() => {
     setSelectedView(view);
   }, [view]);
@@ -150,7 +167,6 @@ const CourseCalendar = ({
   useEffect(() => {
     updateRenderRangeText();
   }, [selectedView, updateRenderRangeText]);
-
 
   return (
     <Box display="flex" flexDirection="column" height="100%">
@@ -189,21 +205,135 @@ const CourseCalendar = ({
         height="900px"
         calendars={initialCalendars}
         month={{ startDayOfWeek: 1 }}
-        events={ calCourses }
+        events={calCourses}
         view={selectedView}
         week={{
           showTimezoneCollapseButton: true,
           timezonesCollapsed: false,
-          eventView: ['time'],
+          eventView: ["time"],
           taskView: false,
         }}
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         ref={calendarRef}
-        useDetailPopup={true}
-      />
+        // useDetailPopup={true}
+        onClickEvent={onClickEvent}
+      />{
+        selectedEvent && (
+          <EventDetailModal
+            event={selectedEvent}
+            isOpen={isEventDetailOpen}
+            onClose={onEventDetailClose}
+          />
+        )
+      }
     </Box>
   );
 };
 
 export default CourseCalendar;
+
+
+// Create all the course events that recur weekly
+function createCalendarEvents(courseScheduleData: Course[]): EventObject[] {
+  const events: EventObject[] = [];
+  var colorIndex = 0;
+
+  courseScheduleData.forEach(course => {
+    const startDate = new Date(course.StartDate);
+    const endDate = new Date(course.EndDate);
+
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= endDate) {
+      var dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase();
+      dayOfWeek = (dayOfWeek === "THU") ? "R" : dayOfWeek.charAt(0); // convert weekday to one letter
+    
+      if (course.Days.includes(dayOfWeek)) {
+        const startTime = convertToTime(course.Begin);
+        const eventStart = currentDate.setHours(parseInt(startTime.split(":")[0]), parseInt(startTime.split(":")[1]), 0);
+        
+        const endTime = convertToTime(course.End);
+        const eventEnd = currentDate.setHours(parseInt(endTime.split(":")[0]), parseInt(endTime.split(":")[1]), 0);
+        
+        const event: EventObject = {
+          id: course.Term.toString() + course.Subj + course.Num.toString() + course.Section, // generate a unique id
+          calendarId: "1",
+          title: course.Subj + ' ' + course.Num.toString() + ' ' + course.Section,
+          body: course.Title,
+          location: course.Bldg + ' ' + course.Room,
+          attendees: [course.Instructor],
+          category: "time",
+          backgroundColor: calendarColours[colorIndex % calendarColours.length].color, 
+          borderColor: calendarColours[colorIndex % calendarColours.length].color,
+          dragBackgroundColor: calendarColours[colorIndex % calendarColours.length].darkColor,
+          start: addDate(new TZDate(eventStart)).toDate().toISOString(),
+          end: addDate(new TZDate(eventEnd)).toDate().toISOString(),
+          isReadOnly: true,
+        };
+
+        events.push(event);
+      }
+
+      currentDate.setDate(currentDate.getDate() + 1);
+      colorIndex++;
+    }
+  });
+
+  return events;
+}
+
+// Courses with two sections that are taught by the same professor are merged 
+function mergeCourses(events: EventObject[]): EventObject[] {
+  const uniqueEvents: EventObject[] = [];
+
+  events.forEach(event => {
+    const isDuplicate = uniqueEvents.some(uniqueEvent =>
+      uniqueEvent.Title === event.Title &&
+      uniqueEvent.Term === event.Term &&
+      uniqueEvent.Subj === event.Subj &&
+      uniqueEvent.Num === event.Num &&
+      uniqueEvent.Instructor === event.Instructor &&
+      uniqueEvent.Bldg === event.Bldg &&
+      uniqueEvent.Room === event.Room &&
+      uniqueEvent.Begin === event.Begin &&
+      uniqueEvent.End === event.End &&
+      uniqueEvent.Days === event.Days
+    );
+
+    if (!isDuplicate) {
+      uniqueEvents.push(event);
+    } else {
+      const existingEvent = uniqueEvents.find(uniqueEvent =>
+        uniqueEvent.Title === event.Title &&
+        uniqueEvent.Term === event.Term &&
+        uniqueEvent.Subj === event.Subj &&
+        uniqueEvent.Num === event.Num &&
+        uniqueEvent.Instructor === event.Instructor &&
+        uniqueEvent.Bldg === event.Bldg &&
+        uniqueEvent.Room === event.Room &&
+        uniqueEvent.Begin === event.Begin &&
+        uniqueEvent.End === event.End &&
+        uniqueEvent.Days === event.Days
+      );
+
+      if (existingEvent) {
+        // Merge lectures by combining the Sections
+        existingEvent.Section += `/${event.Section}`;
+      }
+    }
+  });
+
+  return uniqueEvents;
+}
+
+function addDate(d: TZDate) {
+  const date = clone(d);
+  date.setDate(d.getDate());
+
+  return date;
+};
+
+function clone (date: TZDate): TZDate {
+  return new TZDate(date);
+};
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react";
-import { EventObject, TZDate } from "@toast-ui/calendar";
 import { format, parse } from "date-fns";
-import { Course } from "../types/course";
-import { calendarColours } from "../components/common/sampleData/courseSchedule";
 
 export const useSmallScreen = () => {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
@@ -92,17 +89,6 @@ export const convertIntoReadableRange = (rangeString: string): string => {
   return `${start.month} ${start.day} - ${end.day}, ${start.year}`;
 };
 
-export const addDate = (d: TZDate) => {
-  const date = clone(d);
-  date.setDate(d.getDate());
-
-  return date;
-};
-
-export const clone = (date: TZDate): TZDate => {
-  return new TZDate(date);
-};
-
 // convert time from hhmm to hh:mm
 export const convertToTime = (time: number): string => {
   var timeString = time.toString();
@@ -114,96 +100,3 @@ export const convertToTime = (time: number): string => {
 
   return formattedTime;
 };
-
-// Create all the course events that recur weekly
-export const createCalendarEvents = (courseScheduleData: Course[]): EventObject[] => {
-  const events: EventObject[] = [];
-  var colorIndex = 0;
-
-  courseScheduleData.forEach(course => {
-    const startDate = new Date(course.StartDate);
-    const endDate = new Date(course.EndDate);
-
-    let currentDate = new Date(startDate);
-
-    while (currentDate <= endDate) {
-      var dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase();
-      dayOfWeek = (dayOfWeek === "THU") ? "R" : dayOfWeek.charAt(0); // convert weekday to one letter
-    
-      if (course.Days.includes(dayOfWeek)) {
-        const startTime = convertToTime(course.Begin);
-        const eventStart = currentDate.setHours(parseInt(startTime.split(":")[0]), parseInt(startTime.split(":")[1]), 0);
-        
-        const endTime = convertToTime(course.End);
-        const eventEnd = currentDate.setHours(parseInt(endTime.split(":")[0]), parseInt(endTime.split(":")[1]), 0);
-        
-        const event: EventObject = {
-          id: course.Term.toString() + course.Subj + course.Num.toString() + course.Section, // generate a unique id
-          calendarId: "1",
-          title: course.Subj + ' ' + course.Num.toString() + ' ' + course.Section,
-          body: course.Title,
-          location: course.Bldg + ' ' + course.Room,
-          attendees: [course.Instructor],
-          category: "time",
-          backgroundColor: calendarColours[colorIndex % calendarColours.length].color, 
-          borderColor: calendarColours[colorIndex % calendarColours.length].color,
-          dragBackgroundColor: calendarColours[colorIndex % calendarColours.length].darkColor,
-          start: addDate(new TZDate(eventStart)).toDate().toISOString(),
-          end: addDate(new TZDate(eventEnd)).toDate().toISOString(),
-          isReadOnly: true,
-        };
-
-        events.push(event);
-      }
-
-      currentDate.setDate(currentDate.getDate() + 1);
-      colorIndex++;
-    }
-  });
-
-  return events;
-}
-
-// Courses with two sections that are taught by the same professor are merged 
-export const mergeCourses = (events: EventObject[]): EventObject[] => {
-  const uniqueEvents: EventObject[] = [];
-
-  events.forEach(event => {
-    const isDuplicate = uniqueEvents.some(uniqueEvent =>
-      uniqueEvent.Title === event.Title &&
-      uniqueEvent.Term === event.Term &&
-      uniqueEvent.Subj === event.Subj &&
-      uniqueEvent.Num === event.Num &&
-      uniqueEvent.Instructor === event.Instructor &&
-      uniqueEvent.Bldg === event.Bldg &&
-      uniqueEvent.Room === event.Room &&
-      uniqueEvent.Begin === event.Begin &&
-      uniqueEvent.End === event.End &&
-      uniqueEvent.Days === event.Days
-    );
-
-    if (!isDuplicate) {
-      uniqueEvents.push(event);
-    } else {
-      const existingEvent = uniqueEvents.find(uniqueEvent =>
-        uniqueEvent.Title === event.Title &&
-        uniqueEvent.Term === event.Term &&
-        uniqueEvent.Subj === event.Subj &&
-        uniqueEvent.Num === event.Num &&
-        uniqueEvent.Instructor === event.Instructor &&
-        uniqueEvent.Bldg === event.Bldg &&
-        uniqueEvent.Room === event.Room &&
-        uniqueEvent.Begin === event.Begin &&
-        uniqueEvent.End === event.End &&
-        uniqueEvent.Days === event.Days
-      );
-
-      if (existingEvent) {
-        // Merge lectures by combining the Sections
-        existingEvent.Section += `/${event.Section}`;
-      }
-    }
-  });
-
-  return uniqueEvents;
-}
