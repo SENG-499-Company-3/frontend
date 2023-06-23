54ad601391d62c0dfb685174b0b9c4e55aa6d283
Samantha Lai <slai@uvic.ca>
2023-06-23 14:11:54 -0700

    Adjusted banner to be fixed and appear over profile information

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -25,7 +25,7 @@ const Profile = () => {
     //TODO: Figure out a colour for dark mode and align things properly
     function Banner() {
         return (
-            <div style={{ width: '100%' }}>
+            <div className="fixed w-5/6 mt-2 z-50 place-content-center">
                 <Box sx={{
                     bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#33587A' : '#69B4FA'),
                     mb: 3,
@@ -35,14 +35,14 @@ const Profile = () => {
                     alignItems: 'center',
                     justifyContent: 'space-between'
                 }}>
-                    <div className="flex">
+                    <div className="flex content-center ml-2 gap-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
-                        <div className="gap-2 px-4">You haven't filled out your annual survey yet.
-                        </div>
+                        <Typography variant="body1" gutterBottom>You haven't filled out your annual survey yet.
+                        </Typography>
                     </div>
                     <div className="flex gap-4">
                         <Button variant="outlined" color="inherit">FILL SURVEY</Button>
-                        <button onClick={() => { setShowBanner(false); }}>
+                        <button className={`btn btn-ghost p-1`} onClick={() => { setShowBanner(false); }}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>
                         </button>
                     </div>
@@ -52,7 +52,7 @@ const Profile = () => {
     }
 
     return (
-        <div className="flex flex-col w-full h-full md:px-32 py-8 gap-2">
+        <div className="flex flex-col w-full h-full md:px-16 py-8 gap-2">
             {/*Conditionally render submission reminder box: ADD !userContext.isAdmin() */ }
             {showBanner && <Banner />}
             <div className="flex h-[8rem] w-full justify-center">
