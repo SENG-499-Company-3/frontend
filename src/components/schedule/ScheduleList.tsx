3709fc538f464a79d6f932ca2cb8cbd60966be9a
Samantha Lai <slai@uvic.ca>
2023-06-28 13:26:18 -0700

    Mapped days/times to dropdown in block

--- src/components/schedule/ScheduleList.tsx
+++ src/components/schedule/ScheduleList.tsx
@@ -11,6 +11,7 @@ import SaveIcon from '@mui/icons-material/Save';
 import CancelIcon from '@mui/icons-material/Close';
 import { Button } from '@mui/material';
 
+/* Use index to create unique index numbers required by dataGrid. Stored outside for reuse in createRow, highlighting new rows */
 let index = 1;
 const initialRows: GridRowsProp = courseScheduleData.map((course: Course) => ({
   id: index++,
@@ -29,30 +30,49 @@ const initialRows: GridRowsProp = courseScheduleData.map((course: Course) => ({
 
 const createRow = () => {
     const id = ++index;
-    return { id, days: '', isNew: true };
+    return { id, days: '', isNew: true }; //Update with defaults if desired. Days must be empty string
 };
 
+/* Map incoming schedule's terms and professors into array for dropdowns. CONFIRM this functions with incoming data */
 const termList = courseScheduleData.map((course: Course) => course.Term).filter((value, index, self) => self.indexOf(value) === index);
 const professorList = courseScheduleData.map((course: Course) => course.Instructor).filter((value, index, self) => self.indexOf(value) === index);
 
-//TO DO: finish filling this out and maybe figure out if the setDateTime can map back to one of these dropdown items instead of text override
 const daysTimeSlots = [
-    'MR 08:30 09:50',
-    'MR 10:00 11:20',
-    'MR 13:00 14:20',
-    'TWF 08:30 09:20',
-    'TWF 09:30 10:20',
-    'TWF 11:30 12:20',
+    'MR 08:30 09:50', //A
+    'MR 10:00 11:20', //B
+    'MR 11:30 12:50', //C
+    'MR 13:00 14:20', //D
+    'MWR 14:30 15:20', //E
+    'MWR 15:30 16:20', //F
+    'MW 16:30 17:50', //G
+    'TWF 08:30 09:20', //H
+    'TWF 09:30 10:20', //I
+    'TWF 10:30 11:20', //J
+    'TWF 11:30 12:20', //K
+    'TWF 12:30 13:20', //L
+    'TWF 13:30 14:20', //M
+    'TF 14:30 16:30', //N
+    'TR 16:30 17:50', //O
 ];
 
+/* Getter and Setter for column Block, to use the singular column to update the days, start, and end
+   Note that days/times currently cannot be manually entered. This could be updated by adding blocks VWXYZ (MTWRF) and enable edit on start/end
+   Column block can be overwritten by string */
 function getDaysTime(params: GridValueGetterParams) {
-    return `${params.row.days || ''} ${params.row.start || ''} ${params.row.end || ''}`
+    const inString = `${params.row.days || ''} ${params.row.start || ''} ${params.row.end || ''}`;
+    for (let i = 0; i < 15; i++) {
+        if (inString === daysTimeSlots[i]) {
+            return daysTimeSlots[i];
+        }
+    }
+    return inString;
 }
 function setDaysTime(params: GridValueSetterParams) {
     const [days, start, end] = params.value!.toString().split(' ');
     return { ...params.row, days, start, end };
 }
 
+/* Parser for course, section, and location to force caps entry */
 function parseToCaps(value: any) {
     return String(value).toUpperCase();
 }
@@ -61,6 +81,7 @@ const ScheduleList = () => {
     const [rows, setRows] = useState(initialRows);
     const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
 
+    /* Add rows function for use with the added button */
     const handleAddRow = () => {
         setRows((prevRows) => [...prevRows, createRow()]);
         setRowModesModel((oldModel) => ({
@@ -69,6 +90,7 @@ const ScheduleList = () => {
         }));
     };
 
+    /* Functions from the CRUD table in the documentation */
     const handleEditClick = (id: GridRowId) => () => {
         setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
     };
@@ -109,6 +131,7 @@ const ScheduleList = () => {
         setRowModesModel(newRowModesModel);
     };
 
+    /* Columns moved to be internal due to Actions requiring the above functions */
     const columns: GridColDef[] = [
         { field: 'term', headerName: 'Term', width: 100, editable: true, type:'singleSelect', valueOptions:termList },
         { field: 'course', headerName: 'Course', width: 150, editable: true, valueParser: parseToCaps },
