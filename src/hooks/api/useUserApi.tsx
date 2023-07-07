8e71beb0d7e7a1020546db32e31dfa63014f492e
SlaterRGordon <slats1999@gmail.com>
2023-07-06 19:16:00 -0700

    user api for user endpoints

---
+++ src/hooks/api/useUserApi.tsx
@@ -0,0 +1,28 @@
+import { AxiosInstance, AxiosResponse } from 'axios'
+import { IAuthenticatedUser } from '../../types/auth.d';
+
+const useUserApi = (axios: AxiosInstance) => {
+
+    /**
+     * Registers the user into the application
+     */
+    const create = async (email: string, password: string, name: string, role: RoleType): Promise<AxiosResponse> => {
+        return axios
+            .post('/user/create', { email, password, name, role})
+            .then((response: AxiosResponse<void>) => {
+                console.log(response)
+                if (response.status !== 200) {
+                    console.log("Error creating user")
+                    console.log(response.statusText)
+                }
+
+                return response
+            });
+    }
+
+    return {
+        create,
+    }
+}
+
+export default useUserApi;
