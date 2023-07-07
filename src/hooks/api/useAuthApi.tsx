675ce38d5e1cd8cff394c814d7e0edeb1eb8592e
SlaterRGordon <slats1999@gmail.com>
2023-07-06 19:16:49 -0700

    update api with user api and update auth routes

--- src/hooks/api/useAuthApi.tsx
+++ src/hooks/api/useAuthApi.tsx
@@ -1,15 +1,20 @@
 import { AxiosInstance, AxiosResponse } from 'axios'
 import { IAuthenticatedUser } from '../../types/auth.d';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useContext } from 'react';
 
 const useAuthApi = (axios: AxiosInstance) => {
+    const authContext = useContext(AuthContext);
 
     /**
      * Signs the user into the application by returning their user token
      */
-    const login = async (username: string, password: string): Promise<{ token: string}> => {
+    const login = async (email: string, password: string): Promise<{ token: string}> => {
         return axios
-            .post('/api/login', { username, password})
+            .post('/auth/login', { email, password})
             .then((response: AxiosResponse<string>) => {
+                authContext.setUserToken(response.data)
+
                 return { token: response.data }
             });
     }
@@ -17,17 +22,24 @@ const useAuthApi = (axios: AxiosInstance) => {
     /**
      * Fetches the currently authenticated user
      */
-    const self = async (): Promise<IAuthenticatedUser> => {
+    const self = async (email: string, password: string): Promise<IAuthenticatedUser> => {
         return axios
-            .get('/api/self')
+            .post('/auth/self', {email, password})
             .then((response: AxiosResponse<IAuthenticatedUser>) => {
+                authContext.setCurrentUser(response.data)
                 return response.data
             })
     }
 
+    const logout = async (): Promise<void> => {
+        authContext.resetCurrentUser()
+        authContext.setUserToken(null)
+    }
+
     return {
         login,
-        self
+        self,
+        logout
     }
 }
 
