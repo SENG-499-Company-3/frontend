9145dde2cf8cd9418a97ab6909cf5dda2943472b
SlaterRGordon <slats1999@gmail.com>
2023-07-06 19:17:12 -0700

    remove nav on login/register pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,28 @@
+import { useContext, useEffect } from "react";
 import { Navigation } from "./navigation/Navigation"
+import { AuthContext } from "../contexts/AuthContext";
+import { useRouter } from "next/router";
 
 export default function Layout({ children, switchTheme }) {
+    const authContext = useContext(AuthContext);
+    const router = useRouter();
+
+	useEffect(() => {
+		if (!authContext.isAuthenticated()) {
+			router.push('/login')
+		}
+	}, [AuthContext]);
+
     return (
         <>
-            <Navigation switchTheme={switchTheme} />
-            <main className="flex w-full h-full justify-center items-center box-border pt-[4rem] pb-[4rem] md:pb-0 md:pl-[4.6rem] overflow-y-hidden">{children}</main>
+                { authContext.isAuthenticated() ?
+                    <>
+                        <Navigation switchTheme={switchTheme} />
+                        <main className="flex w-full h-full justify-center items-center box-border pt-[4rem] pb-[4rem] md:pb-0 md:pl-[4.6rem] overflow-y-hidden">{children}</main>
+                    </>
+                    :
+                    <main className="flex w-full h-full justify-center items-center box-border overflow-y-hidden">{children}</main>
+				}
         </>
     );
 }
