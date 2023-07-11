fe2cca08f809cab12e8d2fa5dadbf97f0921c738
Curtis Upshall <curtisupshall@gmail.com>
2023-07-11 10:14:18 -0700

    Added profiles

---
+++ src/pages/profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react"
+import ProfessorProfile from "../components/ProfessorProfile"
+import { AuthContext } from "../contexts/AuthContext"
+
+const ProfessorProfilePage = () => {
+    const authContext = useContext(AuthContext);
+    const currentUser = authContext.currentUser();
+
+    return (
+        <ProfessorProfile
+            name={currentUser.name}
+            email={currentUser.email}
+            canEditCalendar={false}
+            canEditPreferences={true}
+        />
+    )
+}
+
+export default ProfessorProfile
